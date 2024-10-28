6588fc2ecce7bc43d149537e6f9a969f7f8b5806
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-10-28 06:53:49 +0000

    this work contains the details of constant, let and var

---
+++ 03_basics/testing.js
@@ -0,0 +1,17 @@
+console.log("sunny shukla");
+
+const accountId = 12345678
+let accountEmail = "sunnyshukla@gmail.com"
+var accountPassword = "42321"
+accountCity = "Jaipur"
+
+// accountId = 2
+// console.log(accountId);
+
+accountEmail = "sunny@yahoo.com"
+accountPassword = "12121212"
+accountCity = "Mumbai"
+let accountState;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
