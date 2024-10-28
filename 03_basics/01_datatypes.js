33f69373bc5a61afaa5087e40504a473b7d92ae4
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-10-28 11:33:03 +0000

    type of data

--- 03_basics/01_datatypes.js
+++ 03_basics/01_datatypes.js
@@ -1,1 +1,41 @@
 // "use strict"; treat all js code as a newer version 
+
+//alert(3+3) // we are using node js, not browser
+
+//console.log(3+3); console.log("sunny"); // we can also write like this but we must not do this,we must keep things readable.
+
+
+//Data-types in js->
+
+// number => 2 to power 53 (integer)
+// bigInt => for very large data values like in reddit or facebook
+// string => ""
+// boolean => true/false
+// null => standalone value for empty things
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+let myAge = 27
+let myName = "sunny"
+
+console.log(typeof myAge);
+console.log(typeof myName);
+console.log(typeof null); // object at output
+console.log(typeof undefined); //undefined output
+
+
+
+
+
+
+
+
+
+
+
+
+
