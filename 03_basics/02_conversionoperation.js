6cb990c4348763cfb3c1d7fd26e64eada9f80ce2
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-10-28 14:34:42 +0000

    data conversion confusion in js

---
+++ 03_basics/02_conversionoperation.js
@@ -0,0 +1,53 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+//let digit = "33abc"
+//console.log(typeof digit);
+
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//let valueInDigit = Number(digit)
+//console.log(typeof valueInDigit);
+//console.log(valueInDigit);
+
+// let Ascore = null
+// let valueInascore = Number(Ascore)
+// console.log(typeof valueInascore);
+// console.log(valueInascore);
+
+// let Bscore = undefined
+// let valueInBscore = Number(Bscore)
+// console.log(typeof valueInBscore);
+// console.log(valueInBscore);
+
+// let Cscore = true
+// let valueInCscore = Number(Cscore)
+// console.log(typeof valueInCscore);
+// console.log(valueInCscore);
+
+// let Dscore = false
+// let valueInDscore = Number(Dscore)
+// console.log(typeof valueInDscore);
+// console.log(valueInDscore);
+
+// let Escore = "sunny"
+// let valueInEscore = Number(Escore)
+// console.log(typeof valueInEscore);
+// console.log(valueInEscore);
+
+
+
+
+// After Number conversion-> (their type remains a number, but upon printing they come as follows->)
+// 33 => 33
+// 33 abc => NaN
+// true => 1; false => 0
+// null => 0
+
+
