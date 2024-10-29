0cbbde678419d6d4dd874003f8f2580c0d7cc21d
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-10-29 13:40:09 +0000

    we studied about comparison operators in js

---
+++ 03_basics/03_comparison.js
@@ -0,0 +1,43 @@
+// console.log("2" > 1);
+// console.log("02" > 1); 
+// Note:- automatically converts string to number while comparison in js.
+
+//console.log(null > 0); // false
+//console.log(null == 0); // false
+//console.log(null != 0); // true
+//console.log(null >= 0); // true
+//console.log(null <= 0); //true
+
+//  ---- Reason---- >>>>
+// The reason is that an equality check == and comparisons >, <, >=, <= work differently.
+// Comparisons convert null to a number. treating is as o.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined != 0); only true here!
+
+// Strict check ---->>>> (===)
+// console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
