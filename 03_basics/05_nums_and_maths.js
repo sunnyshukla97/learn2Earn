cf5381942befd00c212f24885c31ba27eb4cc281
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-11-01 14:01:30 +0000

    nums

---
+++ 03_basics/05_nums_and_maths.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+// const Balance = new Number(100)
+// console.log(Balance);
+// diff. btw above two is only that when we speciallly give it as a "number", there's a diff. in output.
+
+
+// const newBalanace = new Number(200)
+// console.log(newBalanace);
+// console.log(newBalanace.toString().length);
+// console.log(typeof newBalanace);
+
+// 'to fixed' method - adds decimal value after a number.
+// const activity = 100
+// console.log(activity.toFixed(2)); // syntax for writing.
+
+// 'to precision' method - it precisely reduces a number to closest value like 23.9 to 24 if precise value given is '2' etc.
+// const range = 23.8966
+// console.log(range.toPrecision(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
