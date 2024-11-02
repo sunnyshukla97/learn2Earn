32f174e6d7443aee468bcf39202b1e7a62ad59bf
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-11-02 13:57:36 +0000

    all about nums and maths.

--- 03_basics/05_nums_and_maths.js
+++ 03_basics/05_nums_and_maths.js
@@ -11,13 +11,68 @@
 // console.log(newBalanace.toString().length);
 // console.log(typeof newBalanace);
 
+
+
 // 'to fixed' method - adds decimal value after a number.
 // const activity = 100
-// console.log(activity.toFixed(2)); // syntax for writing.
+// console.log(activity.toFixed(1)); // syntax for writing.
 
 // 'to precision' method - it precisely reduces a number to closest value like 23.9 to 24 if precise value given is '2' etc.
 // const range = 23.8966
 // console.log(range.toPrecision(3));
+
+// 'to locale string' method -> makes a number easily readable(easy to understand, like 1000000, it adds coma and seperates to us or INdian value.)
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); // it sets the number according to US standards.
+// console.log(hundreds.toLocaleString('en-IN')); // it sets the number acc. to Indian standards.
+
+
+
+// +++++++++++++++++ Maths ++++++++++++++++++++ //
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // converts negative to positive. (abs-absolute)
+// console.log(Math.round(4.5)); // converts a number to nearest number, in round figure, removing decimal value.
+// console.log(Math.ceil(4.2)); // converts to nearest higher value. 
+// console.log(Math.floor(4.9)); // converts to small nearest value.
+// console.log(Math.max(4, 3, 2, 6, 9)); // gives the max. value
+// console.log(Math.min(4, 3, 2, 1, 0)); // gives the min. value    
+// console.log(Math.random()); // value comes btw 0 & 1.
+// console.log(Math.random()*10);
+// console.log(Math.random()*10 + 1); // often we add '1' so as to avoid getting value in '0 points something'.
+// console.log((Math.random()*10 + 1)); // easy way to write so as to avoid the repurcussions of bodmas
+// console.log(Math.floor(Math.random()*10) + 1); 
+
+// const min = 10
+// const max = 20
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min); // using min & max so that we can get value btw max & min.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
