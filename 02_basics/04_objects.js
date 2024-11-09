098b0a439cce468b507bf1c7a964fce861e15939
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-11-09 19:37:24 +0000

    done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,17 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "sunny"
+}
+// console.log(course.courseInstructor);
+
+// Another expression of writing the same---->>>>
+// const {courseInstructor} = course
+// console.log(courseInstructor);
+
+// Another simple way of de structure---->>>>
+// const {courseInstructor: instructor} = course
+// console.log(instructor);
+
+
+
