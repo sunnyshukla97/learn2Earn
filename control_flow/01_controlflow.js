e7f3460bc869a8c7c6bed27c7d87c5fa0ecbaddf
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-11-14 14:49:36 +0000

    CONTROL FLOW WITH IF ELIF ELSE.

---
+++ control_flow/01_controlflow.js
@@ -0,0 +1,90 @@
+// if staement! ->
+// if (true){
+//     console.log("hi sunny");
+    
+    
+// }
+// if (false){ // aisa krne pe andar ka code execute nhi hoga!
+//     console.log("hi sunny");
+    
+    
+// }
+
+// if (true){ // ye "true" evaluate kaise hoga?
+//     console.log("hi sunny");
+    
+    
+// }
+
+// if ( 3 != 2 ){ 
+//     console.log("done!");
+    
+// }
+
+// if ( 2 == "2" ){       // yahan hm ()-> iske andar condition check krte hain!
+//     console.log("done!");
+    
+// }
+
+// if ( 2 === "2" ){       // (===)-> triple equal sign ke sath hm strict check krte hain, ispe "type" v check ho jata hai!
+//     console.log("done!");
+    
+// }
+
+// const temperature = 41
+
+// if ( temperature <= 50 ){
+//     console.log("temp is less than 50!");
+    
+// }
+
+// console.log("temp is greater than 50");
+
+// if and else---->>>>
+// const temperature = 41
+
+// if ( temperature === 50 ){
+//     console.log("temp is very hot!");
+    
+// } else {
+//     console.log("temp is moderate!");
+    
+// }
+
+// console.log("code executed!");
+
+// const score = 200
+
+// if ( score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+
+// shorthand notation-> (aise v likha ja sakta hai code, lekin usually hm nhi krte hain ise!)
+
+// const balance = 1000
+
+// if ( balance > 500) console.log("test"), console.log("test2");
+
+// nested if wala case---->>>>>>> (USED WHERE WE NEED TO CHECK MULTIPLE CONDITIONS!)
+
+const balance = 1000
+
+if ( balance < 500){
+    console.log("less than 500");
+    
+} else if ( balance < 750){
+    console.log("less than 750");
+    
+} else if ( balance < 900){
+    console.log("less than 900");
+    
+} else {
+    console.log("balance is 1000");
+    
+}
+
+
+
