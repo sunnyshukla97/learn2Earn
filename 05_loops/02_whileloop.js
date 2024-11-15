06452918574cb4b9e25a0fcf337006ca14bb029d
sunnyshukla97 <shuklasunny1997@gmail.com>
2024-11-15 14:21:19 +0000

    cc

---
+++ 05_loops/02_whileloop.js
@@ -0,0 +1,56 @@
+// While loop-->>
+// syntax->
+
+//  while (condition) {
+    
+//  }
+
+////-> isme hm if ke tarah he -> ()-> isme condition dete hain;
+////-> variable ko loop ke bahar upar me declare karte hain!(initialization)
+////-> aur icrement/decrement hm console.log() ke bad wale line me dete hain!
+////-> for ex->
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index++
+// } 
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// Agar kisi Array ko while loop me krna ho!->
+
+// let myArray = ['superman', 'batman', 'flash']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+//////////++++++++ Do While loop--------->>>>>>>>+++++++++++++++++++++++++++++++++////////////////////////
+// syntax->
+// do { -> do matlab phle isme kam hoga, uske bad condition check hogi!
+    
+// } while (condition);
+
+// ex-1)
+// let score = 1 // yahan kro variable declare
+// do {
+//     console.log(`score is ${score}`); //->yahan kam kro!
+//     score++ // yahan kro increment/decrement
+// } while (score <= 10); // yahan kro condition
+
+
+// ex-2)
+// let score = 11
+// do {
+//     console.log(`score is ${score}`);
+    
+// } while (score <= 10);
